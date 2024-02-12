fa57551c41b799eb1c89c0ed1d51c2c0c92feb49
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-02-12 22:59:54 +0800

    added config colyseus

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import router from "./routes";
 import { errorMiddleware } from "./middlewares/error-middleware";
+import {Server} from "colyseus";
+import {createServer} from "node:http";
 
-const app: Express = express();
+export const app: Express = express();
+
 dotenv.config();
-const PORT = process.env.BASE_PORT || 52718;
-
+const PORT = Number(process.env.BASE_PORT) || 52718;
 
 app.use(express.json());
 app.use(cors({
@@ -19,13 +21,10 @@ app.use(cookieParser());
 app.use('/api', router);
 app.use(errorMiddleware);
 
-const start = (): void => {
-  try {
-    app.listen(PORT, async (): Promise<void> => {
-      console.log(`Server started on ${PORT} port.`);
-    });
-  } catch (e) {
-    console.log(e, 2);
-  }
-};
-start();
+const gameServer = new Server({
+  server: createServer(app)
+})
+
+gameServer.listen(PORT).then(() => {
+  console.log(`Server started on ${PORT} port.`)
+})
